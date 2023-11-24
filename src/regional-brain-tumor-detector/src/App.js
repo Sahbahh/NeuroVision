5b660428b97ab49ca49b75947e3811fc0b450dcc
Risa Kawagoe <rkawagoe@sfu.ca>
2023-11-23 23:07:53 -0800

    implemented loading of model with sample model from ResNet50, able to run model

--- src/regional-brain-tumor-detector/src/App.js
+++ src/regional-brain-tumor-detector/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
+// import * as onnx from "onnxruntime-web";
+import { InferenceSession, Tensor } from "onnxruntime-web";
 
 // components
 import Introduction from './components/Introduction';
@@ -13,11 +15,34 @@ import footerIcon from "./assets/footer-icon.png";
 function App() {
   const [files, setFiles] = useState([])
   const [output, setOutput] = useState(null)
-  const [analyzeBtnEnabled, setanalyzeBtnEnabled] = useState(false)
+  const [ready, setReady] = useState(false)
+  const [session, setSession] = useState(null)
 
   useEffect(() => {
-    setanalyzeBtnEnabled(files.length === 0)
+    async function loadModel() {
+      setSession(await InferenceSession.create(
+        'https://microsoft.github.io/onnxjs-demo/resnet50v2.onnx',// replace with our model
+        {
+          executionProviders: ["webgl"],
+        }
+      ))
+    }
+    loadModel()
+
+  }, [])
+
+  useEffect(() => {
+    setReady(files.length > 0)
   }, [files])
+  useEffect(() => {
+    if(output !== null && output !== undefined) {
+      console.log("output:")
+      console.log(output)
+      // const prediction = output.resnetv24_dense0_fwd.data
+      // console.log(prediction.indexOf(Math.max(...prediction)))
+    }
+  }, [output])
+
 
   const removeFile = (index) => {
     const copiedList = files
@@ -25,10 +50,65 @@ function App() {
     setFiles([...copiedList])
   }
 
-  const runModel = () => {
-    alert("Running model: currently displaying dummy result. Output has to be replaced with the actual output from the model.")
-    console.log("running model")
-    setOutput({})
+
+  const runModel = async () => {
+    if(session === null) {
+      alert("Machine learning model has not been loaded yet. Please try again.")
+      return
+    }
+
+    if(!ready) {
+      alert("ERR: input images not provided")
+      return
+    }
+
+    alert("Warning!\nCurrently running ResNet50 from microsoft. Output has to be replaced with the actual output from our model.\nUpload 'sample-input-224x224.png' in regional-brain-tumor-detector/assets/ or any image file with dimension of 224x224 to test out this feature.")
+
+    try {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const image = new Image()
+        image.src = reader.result
+        image.onload = async () => {
+
+          const EXPECTED_WIDTH = 224;
+          const EXPECTED_HEIGHT = 224;
+          const width = image.width
+          const height = image.height
+
+          if(!(width === EXPECTED_WIDTH && height === EXPECTED_HEIGHT)) {
+            alert("ERR: dimension of image has to be 324 x 324")
+            return
+          }
+
+          const channels = 3; // RGB image
+          const canvas = document.createElement("canvas")
+          canvas.width = width
+          canvas.height = height
+          const context = canvas.getContext("2d")
+          context.drawImage(image, 0, 0, width, height)
+          const imageData = context.getImageData(0, 0, width, height)
+
+          const inputArray = new Float32Array(width * height * channels)
+          for (let i = 0; i < width * height * channels; i++) {
+            inputArray[i] = imageData.data[i] / 255.0;
+          }
+          
+          const inputDimensions = [1, channels, EXPECTED_WIDTH, EXPECTED_HEIGHT];
+          const inputTensor = new Tensor("float32", inputArray, inputDimensions);
+
+          const feeds = { data: inputTensor };
+          setOutput(await session.run(feeds))
+        }
+      }
+      
+      reader.readAsDataURL(files[0]);
+    }catch(error) {
+      console.log(error)
+      alert(error)
+    }
+    // setOutput({})
   }
 
 
@@ -39,7 +119,7 @@ function App() {
         <FileUploader fileList={files} updateFiles={setFiles} />
         <FilePreviewer fileList={files} removeFile={removeFile} />
         <section id="analysisControls">
-          <button id="analyzeBtn" disabled={analyzeBtnEnabled} onClick={runModel}>Analyze</button>
+          <button id="analyzeBtn" disabled={!ready} onClick={runModel}>Analyze</button>
         </section>
         {output && <AnalysisDisplayer />}
       </main>
